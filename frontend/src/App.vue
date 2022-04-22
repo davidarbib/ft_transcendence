e40718bae64a4f2c116ad6a3107f81d2844d98a9
macrespo42 <macrespo@student.42.fr>
2022-04-22 16:12:08 +0200

    move tailwind css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,7 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import './css/tailwind.css'
+import "./assets/tailwind.css";
 
 import App from "./App.vue";
 import router from "./router";
