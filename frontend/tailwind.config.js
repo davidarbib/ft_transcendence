5d90d2491157affbf1e4dd16c5b9ddf3ca022be2
macrespo42 <macrespo@student.42.fr>
2022-04-22 16:05:13 +0200

    tailwind installed

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./App.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
