0f56bc1ce6d5caac6d400163f6e44d27baf92ac2
jurichar <julienrichard98@gmail.com>
2022-04-25 09:18:53 -0700

    coucou

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,9 +1,84 @@
+<script>
+import { LockClosedIcon } from "@heroicons/vue/solid";
+
+export default {
+  components: {
+    LockClosedIcon,
+  },
+};
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is register page</h1>
+  <div id="signin-section">
+    <div>
+      <h1 class="title">
+        <span class="metal clip raise">Retro</span>
+        <span class="metal clip raise">Effect</span>
+      </h1>
+      <h2 class="subtitle">With SCSS</h2>
+    </div>
+    <div
+      class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
+    >
+      <div class="max-w-md w-full space-y-8">
+        <form class="mt-8 space-y-6" action="#" method="POST">
+          <input type="hidden" name="remember" value="true" />
+          <div class="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label for="email-address" class="sr-only">Email address</label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autocomplete="email"
+                required=""
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+              />
+            </div>
+            <div>
+              <label for="password" class="sr-only">Password</label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autocomplete="current-password"
+                required=""
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+
+          <div class="auth-buttons">
+            <router-link to="/main">
+            <button
+              type="submit"
+              class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Sign in
+            </button>
+            </router-link>
+
+            <router-link to="/register">
+            <button
+              class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Register
+            </button>
+            </router-link>
+
+            <button id="ConnectWith42"
+              class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Connect with 42
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
-
 <style>
 @media (min-width: 1024px) {
   .about {
