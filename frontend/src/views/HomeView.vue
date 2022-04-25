64852395a4f7342c0d8827cc4027b5b3b76f2faf
macrespo42 <macrespo@student.42.fr>
2022-04-25 19:46:21 +0200

    title component

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,16 +1,11 @@
 <script setup lang="ts">
 import LockClosedIcon from "@heroicons/vue/solid";
+import Title from "@/components/Title.vue";
 </script>
 
 <template>
+  <Title/>
   <div id="signin-section">
-    <div>
-      <h1 class="title">
-        <span class="metal clip raise">Retro</span>
-        <span class="metal clip raise">Effect</span>
-      </h1>
-      <h2 class="subtitle">With SCSS</h2>
-    </div>
     <div
       class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
     >
@@ -25,7 +20,7 @@ import LockClosedIcon from "@heroicons/vue/solid";
                 name="email"
                 type="email"
                 autocomplete="email"
-                required=""
+                required=true
                 class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
               />
@@ -37,7 +32,7 @@ import LockClosedIcon from "@heroicons/vue/solid";
                 name="password"
                 type="password"
                 autocomplete="current-password"
-                required=""
+                required=true
                 class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
               />
@@ -82,19 +77,6 @@ import LockClosedIcon from "@heroicons/vue/solid";
   transform: translateY(-50%);
 }
 
-h1 {
-  font-family: Kallisto-bold, sans-serif;
-  text-transform: uppercase;
-  background: -webkit-linear-gradient(
-    0.25turn,
-    v.$primary,
-    v.$dark-blue,
-    v.$primary
-  );
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-}
-
 .auth-buttons {
   display: flex;
   flex-flow: column nowrap;
@@ -111,41 +93,5 @@ h1 {
   #ConnectWith42 {
     background-color: black;
   }
-}
-
-.metal {
-  background: linear-gradient(
-    hsl(239, 50%, 30%) 15%,
-    hsl(239, 50%, 40%) 25%,
-    hsl(200, 60%, 50%) 35%,
-    hsl(100, 70%, 80%) 45%,
-    hsl(60, 100%, 98%) 50%,
-    hsl(240, 0%, 0%) 52%,
-    hsl(240, 10%, 10%) 60%,
-    hsl(240, 50%, 30%) 70%,
-    hsl(220, 70%, 60%) 80%,
-    hsl(212, 92%, 76%) 85%
-  );
-
-  color: transparent;
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-stroke: 2px hsl(200, 65%, 80%);
-}
-
-.raise {
-  filter: drop-shadow(1px 3px 0px hsl(242, 54%, 10%))
-    drop-shadow(1px 3px 0px hsl(242, 54%, 15%))
-    drop-shadow(1px 3px 0px hsl(242, 54%, 20%))
-    drop-shadow(1px 3px 0px hsl(242, 54%, 25%))
-    drop-shadow(1px 3px 0px hsl(242, 54%, 30%))
-    drop-shadow(0px 0px 10px rgba(16, 16, 16, 0.4));
-}
-
-.subtitle {
-  transform: rotateZ(-10deg);
-  color: hsl(318, 100%, 51%); /* pink */
-  text-shadow: -2px -3px hsl(53, 100%, 81%) /* yellow */,
-    5px 5px hsl(240, 90%, 61%); /* blue */
 }
 </style>
