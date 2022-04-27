4b987ba044d3da42acf95174a0ebf888bfb47d69
macrespo42 <macrespo@student.42.fr>
2022-04-28 01:29:10 +0200

    home play music

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,6 +4,11 @@ import Title from "@/components/Title.vue";
 </script>
 
 <template>
+  <div class="sound-icon">
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" />
+    </svg>
+  </div>
   <div id="signin-section">
     <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div class="max-w-md w-full space-y-8">
@@ -65,10 +70,22 @@ import Title from "@/components/Title.vue";
       </div>
     </div>
   </div>
+  <audio controls id="myVideo" autoplay loop muted hidden>
+  <source src="@/assets/homepage_music.mp3" type="audio/mpeg">
+  Your browser does not support the audio element.
+  </audio>
+
 </template>
 
 <style lang="scss">
 @use "../assets/variables.scss" as v;
+
+ .sound-icon {
+   position: absolute;
+   top: 1rem;
+   right: 2rem;
+   font-size: 10rem;
+ }
 
 #signin-section {
   form {
