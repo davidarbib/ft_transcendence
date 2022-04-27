b21a320b8c0048afe9e7c48a262f277263eba1cf
jurichar <julienrichard98@gmail.com>
2022-04-27 07:53:54 -0700

    navbar v5 fini ?

--- frontend/src/components/NavbarItem.vue
+++ frontend/src/components/NavbarItem.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-  import { ref } from "vue";
+import { ref } from "vue";
 
-  const visible = ref(true);
+const visible = ref(true);
 
-  const visibleToggle = () => { visible.value = !visible.value }
+const visibleToggle = () => {
+  visible.value = !visible.value;
+};
 </script>
 
 <template>
@@ -12,13 +14,14 @@
       <div class="flex space-x-7">
         <div>
           <!-- Website Logo -->
-          <a class="flex items-center py-0 px-2">
+          <router-link to="/main" class="flex items-center py-0 px-2">
             <img src="@/assets/logo.png" alt="Logo" class="h-14 w-28 mr-2" />
-          </a>
+          </router-link>
         </div>
         <!-- Primary Navbar items -->
         <div class="hidden md:flex items-center space-x-1">
-          <a
+          <router-link
+            to="/chat"
             class="
               py-4
               px-2
@@ -26,20 +29,12 @@
               font-semibold
               hover:text-[#e63380]
               transition
-              duration-300
-            ">Chat</a>
-          <a
-            class="
-              py-4
-              px-2
-              text-gray-300
-              font-semibold
-              hover:text-[#e63380]
-              transition
-              duration-300
+              duration-700
             "
-            >Stats</a>
-          <a
+            >Chat</router-link
+          >
+          <router-link
+            to="/stats"
             class="
               py-4
               px-2
@@ -47,10 +42,12 @@
               font-semibold
               hover:text-[#e63380]
               transition
-              duration-300
+              duration-700
             "
-            >Tutorial</a>
-          <a
+            >Stats</router-link
+          >
+          <router-link
+            to="/tuto"
             class="
               py-4
               px-2
@@ -58,25 +55,40 @@
               font-semibold
               hover:text-[#e63380]
               transition
-              duration-300
+              duration-700
             "
-            >Profil</a>
+            >Tutorial</router-link
+          >
+          <router-link
+            to="/profil"
+            class="
+              py-4
+              px-2
+              text-gray-300
+              font-semibold
+              hover:text-[#e63380]
+              transition
+              duration-700
+            "
+            >Profil</router-link
+          >
         </div>
       </div>
       <!-- Secondary Navbar items -->
       <div class="hidden md:flex items-center space-x-3">
-        <a
+        <router-link
+          to="/"
           class="
             py-2
             px-2
             font-medium
             text-gray-300
             rounded
-            hover:bg-[#e63380]
+            hover:bg-red-700
             transition
-            duration-300
+            duration-700
           "
-          >Disconnect</a
+          >Disconnect</router-link
         >
       </div>
       <!-- Mobile menu button -->
@@ -101,56 +113,68 @@
   <!-- mobile menu -->
   <div class="mobile-menu" :class="{ hidden: visible }">
     <ul class="">
-      <li class="active">
-        <a
-          href="index.html"
-          class="block text-sm px-2 py-4 text-white bg-green-500 font-semibold"
-          >Home</a
-        >
-      </li>
       <li>
-        <a
-          href="#services"
+        <router-link
+          to="/chat"
           class="
             block
             text-sm
             px-2
             py-4
-            hover:bg-green-500
+            text-gray-300
+            hover:bg-[#e63380]
             transition
-            duration-300
+            duration-700
           "
-          >Services</a
+          >Chat</router-link
         >
       </li>
       <li>
-        <a
-          href="#about"
+        <router-link
+          to="/stats"
           class="
             block
             text-sm
             px-2
             py-4
-            hover:bg-green-500
+            text-gray-300
+            hover:bg-[#e63380]
             transition
-            duration-300
+            duration-700
           "
-          >About</a
+          >Stats</router-link
         >
       </li>
       <li>
-        <a
-          href="#contact"
+        <router-link
+          to="/tuto"
           class="
             block
             text-sm
             px-2
             py-4
-            hover:bg-green-500
+            text-gray-300
+            hover:bg-[#e63380]
             transition
-            duration-300
+            duration-700
           "
-          >Contact Us</a
+          >Tutorial</router-link
+        >
+      </li>
+      <li>
+        <router-link
+          to="/profil"
+          class="
+            block
+            text-sm
+            px-2
+            py-4
+            text-gray-300
+            hover:bg-[#e63380]
+            transition
+            duration-700
+          "
+          >Profil</router-link
         >
       </li>
     </ul>
