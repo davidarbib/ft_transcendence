8835d00cdae26475c5848ce03afe4a6d74bb27b0
jurichar <julienrichard98@gmail.com>
2022-04-27 09:30:02 -0700

    contact bar v0

---
+++ frontend/src/components/Contact.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+    import {ref} from "vue"
+    import json from "@/assets/dataTest.json"
+    console.log(json);
+    const online = ref(false)
+</script>
+
+<template>
+    <div class="container w-3/12 h-screen">
+        <input placeholder="search" class="w-full">
+        <div v-for="user in json" :key="user.id" class="name">
+            {{user.last_name}}
+            <div class="online"> </div>
+            <div class="offline"> </div>
+            <div class="ingame"> </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+    border: 0.1rem crimson solid;
+    overflow: scroll;
+}
+.name {
+    border: 0.1rem blue solid;
+}
+.online {
+    background-color: aqua;
+}
+</style>
--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import NavbarItem from "@/components/NavbarItem.vue";
+import Contact from "@/components/Contact.vue";
 </script>
 
 <template>
   <NavbarItem/>
+  <Contact/>
   <div class="about">
     <h1>This is main page</h1>
   </div>
