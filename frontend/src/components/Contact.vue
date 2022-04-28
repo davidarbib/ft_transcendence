2fb0b9efecf27d3c412f3844770a309272ce41b3
macrespo42 <macrespo@student.42.fr>
2022-04-28 15:58:16 +0200

    round friendList component

--- frontend/src/components/Contact.vue
+++ frontend/src/components/Contact.vue
@@ -13,7 +13,7 @@
 <template>
     <div class="container w-2/5 mx-2 h-auto">
         <input placeholder="search" class="w-full rounded px-2" v-model="searched">
-        <div v-for="user in userFriends" :key="user.id" class="name my-2 w-full bg-black bg-opacity-10" :class="{ online: user.status === true, offline: user.status === false}">
+        <div v-for="user in userFriends" :key="user.id" class="name my-2 w-full bg-black bg-opacity-10 rounded" :class="{ online: user.status === true, offline: user.status === false}">
         <div class="mx-3  status-composant">
             <img class="h-8 w-8 rounded-full" src="@/assets/sphere.png" alt="">
             <i class="fa-solid fa-circle" :class="{ online: user.status === true, offline: user.status === false}"></i>
