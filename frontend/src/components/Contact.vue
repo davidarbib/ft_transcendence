b8619aae85325f3b0ff366d97d47ceb219b3a7fb
macrespo42 <macrespo@student.42.fr>
2022-04-28 15:57:08 +0200

    friendList basic styles

--- frontend/src/components/Contact.vue
+++ frontend/src/components/Contact.vue
@@ -11,33 +11,34 @@
 </script>
 
 <template>
-    <div class="container w-3/12 h-auto">
-        <input placeholder="search" class="w-full" v-model="searched">
-        <div v-for="user in userFriends" :key="user.id" class="name w-full" :class="{ online: user.status === true, offline: user.status === false}">
-        <div class="status-composant">
+    <div class="container w-2/5 mx-2 h-auto">
+        <input placeholder="search" class="w-full rounded px-2" v-model="searched">
+        <div v-for="user in userFriends" :key="user.id" class="name my-2 w-full bg-black bg-opacity-10" :class="{ online: user.status === true, offline: user.status === false}">
+        <div class="mx-3  status-composant">
             <img class="h-8 w-8 rounded-full" src="@/assets/sphere.png" alt="">
             <i class="fa-solid fa-circle" :class="{ online: user.status === true, offline: user.status === false}"></i>
         </div>
-            <h3>{{user.pseudo}}</h3>
+            <h3> <span class="friend-pseudo"> {{user.pseudo}} </span> <br/> <span class="status-text">online</span></h3>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
 .container {
-    overflow: scroll;
+    overflow: scroll;;
     .status-composant {
         display: flex;
         justify-content: flex-start;
 
         .fa-circle {
+            font-size: 0.8rem;
             position: relative;
             top: 1rem;
             right: 0.5rem;
         }
     }
     .online {
-        color: green;
+        color: rgb(15, 164, 70);
     }
     .offline {
         color: gray;
@@ -47,6 +48,18 @@
     }
 }
 .name {
-    border: 2px solid blue;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: baseline;
+    align-items: center;
+
+    .friend-pseudo {
+        color: rgb(160, 165, 141);
+        position: relative;
+        top: 0.5rem;
+    }
+    .status-text {
+        font-size: 0.8rem;
+    }
 }
 </style>
