0f1834d3850f71aa1fd304d67d70b12527612e32
macrespo42 <macrespo@student.42.fr>
2022-05-12 17:37:39 +0200

    draw game board + element

--- frontend/src/components/BlockGame.vue
+++ frontend/src/components/BlockGame.vue
@@ -1,30 +1,64 @@
 <template>
-    <div id="canvas">
-        <!-- <canvas 
-            id = 'canvas'
-            ref="game"
-            width="640" 
-            height="480" 
-            style="border: 5px solid black;">
-        </canvas> -->
-    </div>
+  <div class="game-section">
+    <canvas
+      id="pong"
+      ref="canvaRef"
+      :width="width"
+      :height="height"
+      style="background-color: black"
+    >
+    </canvas>
+  </div>
 </template>
 
 <script setup lang="ts">
-function draw () 
-{
-    var canvas = document.getElementById('canvas');
-    if (canvas.getContext)
-    {
-        var ctx = canvas.getContext('2d');
+import { faDrawPolygon } from "@fortawesome/free-solid-svg-icons";
+import io from "socket.io";
+import { ref, onMounted } from "vue";
 
-        ctx.fillRect(25, 25, 100, 100);
-    }
+const canvaRef = ref<HTMLCanvasElement | null>(null);
+const width = ref(700);
+const height = ref(700);
+
+function draw_paddle(x: number, y: number): void {
+  const ctx = ref(canvaRef.value?.getContext("2d"));
+  ctx.value!.fillStyle = "#FFFFFF";
+  ctx.value?.fillRect(x, y, 20, 60);
 }
 
-draw()
+function draw_ball(x: number, y: number): void {
+  const ctx = ref(canvaRef.value?.getContext("2d"));
+  ctx.value!.fillStyle = "#FFFFFF";
+  ctx.value?.fillRect(x, y, 20, 20);
+}
+
+function draw_middle_line(x: number, y: number) {
+  const ctx = ref(canvaRef.value?.getContext("2d"));
+  ctx.value!.fillStyle = "#FFFFFF";
+  ctx.value?.fillRect(x, y, 10, 10);
+}
+
+onMounted(() => {
+  draw_paddle(10, height.value / 2);
+  draw_paddle(width.value - 30, height.value / 2);
+  draw_ball(width.value / 1.6, height.value / 2);
+  for (
+    let middle_line_height = height.value;
+    middle_line_height > 0;
+    middle_line_height -= 20
+  ) {
+    draw_middle_line(width.value / 2, middle_line_height);
+  }
+});
 </script>
 
-
 <style scoped lang="scss">
-</style> 
+.game-section {
+  width: 100%;
+  text-align: center;
+
+  #pong {
+    display: inline;
+  }
+}
+</style>
