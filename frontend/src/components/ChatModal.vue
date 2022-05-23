62552b2cadab30a4ab99292fe75101a5bc407ac5
macrespo42 <macrespo@student.42.fr>
2022-05-23 18:28:41 +0200

    chat modal js work

---
+++ frontend/src/components/ChatModal.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const open = ref(false)
+</script>
+
+<template>
+  <button @click="open = true" class="secondary-button text-left mx-1"><slot/></button>
+  <Teleport to="body">
+    <div v-if="open" class="modal">
+      <slot/>
+      <button @click="open = false"><slot/></button>
+    </div>
+  </Teleport>
+</template>
+
+<style scoped>
+.modal {
+  position: fixed;
+  z-index: 999;
+  top: 20%;
+  left: 50%;
+  width: 300px;
+  margin-left: -150px;
+}
+</style>
