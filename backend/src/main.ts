f5ec5d25cc25faf70d79c1d4ee4c06ad134aa51f
David Arbib <davidarbib@yahoo.fr>
2022-06-01 18:23:51 +0200

    basic services for match and player

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as session from 'express-session'
 import * as passport from 'passport'
 
 import { ValidationPipe } from '@nestjs/common';
@@ -10,7 +9,7 @@ async function bootstrap()
 {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT;
-  app.useGlobalPipes(new ValidationPipe()) 
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true})); 
 
   const config = new DocumentBuilder()
     .setTitle('API example')
@@ -23,23 +22,12 @@ async function bootstrap()
   const doc = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, doc);
 
-  app.use(
-    session({
-      cookie: {
-        maxAge: 86400000, //1 day
-      },
-      secret: process.env.COOKIE_SECRET,
-      resave: false,
-      saveUninitialized: false,
-    }),
-  );
   app.use(passport.initialize());
-  app.use(passport.session());
 
   app.enableCors({
     credentials:true,
     origin:true
-  }) // pour connecter le back et le front !!!
+  }); // pour connecter le back et le front !!!
 
   await app.listen(PORT, () => console.log(`Running on Port : ${PORT}`));
 }
