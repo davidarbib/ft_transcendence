89ccf2a430b0c11dee7ba9d412a80890c28f532b
David Arbib <davidarbib@yahoo.fr>
2022-06-09 01:34:04 +0200

    2fa secret are now encrypted

--- backend/src/auth/services/twoFactorAuth.service.ts
+++ backend/src/auth/services/twoFactorAuth.service.ts
@@ -6,15 +6,12 @@ import { TwoFactorSecret } from 'src/utils/types';
 import { Response } from 'express';
 import { toFileStream } from 'qrcode';
 import { JwtService } from '@nestjs/jwt';
+import { encrypt, decrypt } from 'src/utils/encryption';
 
 @Injectable()
 export class TwoFactorAuthService
 {
-    constructor
-    (
-        private usersService : UsersService,
-        private jwtService: JwtService,
-    )
+    constructor(private usersService : UsersService)
     { }
 
     public async generateTwoFactorSecret(user: User) : Promise<TwoFactorSecret>
@@ -23,14 +20,27 @@ export class TwoFactorAuthService
             name: process.env.APP_NAME_2FA,
             account: user.login,
         });
+        
+        const encryptedSecret = encrypt(
+            secret.secret,
+            process.env.TWOFA_KEY,
+            process.env.TWOFA_IV
+        );
 
-        await this.usersService.setTwoFactorSecret(user.id, secret.secret);
+        //await this.usersService.setTwoFactorSecret(user.id, secret.secret);
+        await this.usersService.setTwoFactorSecret(user.id, encryptedSecret);
         return secret;
     }
 
     public async isTwoFactorAuthValid(token: string, user: User) : Promise<boolean>
     {
-        const result = node2fa.verifyToken(user.twoFactorSecret, token);
+        const secret = decrypt(
+            user.twoFactorSecret,
+            process.env.TWOFA_KEY,
+            process.env.TWOFA_IV
+        );
+        const result = node2fa.verifyToken(secret, token);
+        //const result = node2fa.verifyToken(user.twoFactorSecret, token);
         if (!result)
             return false;
         const { delta } = result;
