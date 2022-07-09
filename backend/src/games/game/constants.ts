492c04557575317dca320411dfe4b2662dd7cfca
David Arbib <davidarbib@yahoo.fr>
2022-07-09 15:23:43 +0200

    core gameplay done

--- backend/src/games/game/constants.ts
+++ backend/src/games/game/constants.ts
@@ -1,11 +1,13 @@
 const PADGAPX = 5;
+export const PADGAPY = 0;
 
 export const WIDTH=100;
 export const HEIGHT=100;
 
 export const PADSIZE=15;
 export const PADWIDTH=2;
-export const PADHEIGHT=15;
+export const PADHEIGHT=PADSIZE;
+export const HALFPAD=PADHEIGHT/2;
 export const P1PADX=PADGAPX;
 export const P2PADX=WIDTH - PADGAPX;
 export const PADY=HEIGHT/2;
@@ -15,6 +17,7 @@ export const BALLSIZE=3;
 export const BALLVELOCITY=1;
 export const BALLINITX=WIDTH/2;
 export const BALLINITY=HEIGHT/2; 
+export const HALFBALL=BALLSIZE/2; 
 
 export const WINTHRESH=11;
 export const HANDICAP=0;
