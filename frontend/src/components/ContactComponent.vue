57e0d8dc34c89634a04f07af0c4aaa41d9e6805a
macrespo42 <macrespo@student.42.fr>
2022-07-11 15:10:20 +0200

    cut ContactComponent im more generic + connect with back

--- frontend/src/components/ContactComponent.vue
+++ frontend/src/components/ContactComponent.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 import axios from "axios";
-//import { useUserStore } from "@/stores/auth";
 import { apiStore } from "@/stores/api";
+import ListUserComponent from "@/components/ListUserComponent.vue";
 
 interface User {
   id: string;
@@ -19,70 +19,35 @@ interface User {
 }
 
 const api = apiStore();
-//const userStore = useUserStore();
-//const friends = ref<User[]>([]);
+const friends = ref<User[]>([]);
 let users = ref<User[]>([]);
-const friendMenu = ref(false);
-const friendSelected = ref<string>("");
-
-axios.defaults.withCredentials = true;
-
-function toggleFriendMenu(id: string) {
-  friendSelected.value = id;
-  friendMenu.value = !friendMenu.value;
-}
 
 onMounted(() => {
   axios
     .get(`${api.url}/users`)
     .then((response) => {
-      console.log(response.data);
       users.value = response.data;
     })
     .catch((error) => {
       console.log(error);
     });
-})
+
+  axios
+    .get(`${api.url}/contacts`)
+    .then((response) => {
+      friends.value = response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 </script>
 
 <template>
   <div class="contact-section">
-    <div
-      class="user-card rounded my-2 bg-black bg-opacity-10 font-medium hover:bg-opacity-30 transition duration-300"
-      v-for="user in users"
-      :key="user.id"
-      @click="toggleFriendMenu(user.id)"
-    >
-      <div class="user-icon">
-        <img src="@/assets/sphere.png" alt="" class="h-10 w-10" />
-        <p v-if="user.status" class="online">
-          <i class="fa-solid fa-circle status-dot outline-inherit"></i>
-        </p>
-        <p v-else class="offline">
-          <i class="fa-solid fa-circle status-dot outline-inherit"></i>
-        </p>
-      </div>
-      <div class="user-pseudo py-2">
-        <p>{{ user.username }}</p>
-        <p v-if="user.status" class="online">online</p>
-        <p v-else class="offline">offline</p>
-        <Transition name="slide-fade">
-          <div v-if="friendMenu && friendSelected === user.id">
-            <ul class="list">
-              <li><router-link to="/chat">chat</router-link></li>
-              <li>
-                <router-link :to="'/profile/' + user.username"
-                  >profile</router-link
-                >
-              </li>
-              <li><router-link to="/">invite</router-link></li>
-              <li><router-link to="/">block</router-link></li>
-              <li><router-link to="/">spectate</router-link></li>
-            </ul>
-          </div>
-        </Transition>
-      </div>
-    </div>
+    <list-user-component :users="users" />
+    <div class="sep"></div>
+    <list-user-component :users="friends" />
   </div>
 </template>
 
@@ -93,62 +58,8 @@ onMounted(() => {
   height: 92vh;
   overflow: scroll;
 
-  .list {
-    list-style: inside;
-
-    li:hover {
-      color: white;
-    }
-  }
-
-  .user-card {
-    cursor: pointer;
-    display: grid;
-    grid-template-columns: 20% 1fr;
-    grid-template-rows: 10% 1fr;
-    -webkit-touch-callout: none; /* iOS Safari */
-    -webkit-user-select: none; /* Safari */
-    -moz-user-select: none; /* Old versions of Firefox */
-    -ms-user-select: none; /* Internet Explorer/Edge */
-    user-select: none; /* Non-prefixed version, currently */
-    .user-icon {
-      padding-top: 1rem;
-      padding-left: 0.4rem;
-      grid-area: 2 / 1 / 3 / 2;
-      .status-dot {
-        position: relative;
-        top: -1.3rem;
-        right: -1.8rem;
-      }
-    }
-    .user-pseudo {
-      color: v.$primary;
-      padding-left: 0.5rem;
-      // background-color: orange;
-      grid-area: 2 / 2 / 3 / 3;
-    }
-
-    .online {
-      color: green;
-    }
-
-    .offline {
-      color: gray;
-    }
-  }
-
-  .slide-fade-enter-active {
-    transition: all 0.3s ease-out;
-  }
-
-  .slide-fade-leave-active {
-    transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1);
-  }
-
-  .slide-fade-enter-from,
-  .slide-fade-leave-to {
-    transform: translateX(20px);
-    opacity: 0;
+  .sep {
+    border-bottom: 10px solid v.$primary;
   }
 }
 </style>
