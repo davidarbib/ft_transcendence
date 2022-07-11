880b994676e4a924e8849c23565a4b5e6e1afed1
David Arbib <davidarbib@yahoo.fr>
2022-07-11 17:19:01 +0200

    switchStatus done for online offline

---
+++ backend/src/users/users.gateway.ts
@@ -0,0 +1,36 @@
+
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets'
+import { Server } from 'socket.io';
+import { User, UserStatus } from './entities/user.entity';
+import { UsersService } from './users.service';
+
+interface StatusSwitchPayload
+{
+    userId: string,
+    status: string
+}
+
+@WebSocketGateway({
+  cors: {
+    origin: "http://localhost:8000",
+    methods: ["GET", "POST"],
+  }
+})
+export class UsersGateway {
+
+  @WebSocketServer()
+  server : Server;
+
+  constructor( private readonly usersService: UsersService )
+  { }
+
+  async handleStatusSwitch(userId: string, status: UserStatus)
+  {
+    await this.usersService.switchStatus(userId, status);
+    const payload : StatusSwitchPayload = {
+        userId: userId,
+        status: status,
+    }
+    this.server.emit("switchStatus", payload);
+  }
+}
